bdea8f2e4ef6c44df28dd23ecbc42123494d48f2
tonymmm1 <tony@magnatox.com>
2024-08-28 08:50:56 -0500

    remove unused cors middleware

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -7,10 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
-
-var middlewareHandler *Handler
 
 // fix for Google
 func ReferrerPolicyMiddleware(next http.Handler) http.Handler {
@@ -36,19 +33,6 @@ func CustomLogger(next http.Handler) http.Handler {
 }
 
 func Middleware(m *Handler, r chi.Router) {
-	middlewareHandler = m
-	//CORS
-	corsOptions := cors.Options{
-		AllowedOrigins:   []string{"https://www.sandbox.paypal.com"}, // Use specific origins in production
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}
-
-	// Middleware
-	r.Use(cors.Handler(corsOptions))
 	r.Use(middleware.AllowContentEncoding("deflate", "gzip")) // AllowContentEncoding middleware will allow the client to request compressed content
 	r.Use(middleware.CleanPath)                               // CleanPath middleware will clean up the request URL path
 	r.Use(middleware.Heartbeat("/api"))                       // Add a heartbeat endpoint
