a70d799a95d382c84e41fd967337b9c1c6bad45b
tonymmm1 <tony@magnatox.com>
2024-11-29 23:12:40 -0600

    add problems support and use base64 for code, hints

--- src/sql/queries/problems.sql
+++ src/sql/queries/problems.sql
@@ -1,2 +1,29 @@
+-- name: CreateProblem :one
+INSERT INTO problem (title, description, prompt, points, tags) VALUES ($1, $2, $3, $4, $5) RETURNING id;
+
+-- name: CreateProblemCode :exec
+INSERT INTO problem_code (problem_id, language, code) VALUES ($1, $2, $3);
+
+-- name: CreateProblemHint :exec
+INSERT INTO problem_hint (problem_id, description, answer) VALUES ($1, $2, $3);
+
+-- name: GetProblem :one
+SELECT * FROM problem WHERE id = $1;
+
+-- name: GetProblems :many
+SELECT * FROM problem;
+
+-- name: GetProblemCodes :many
+SELECT * FROM problem_code WHERE problem_id = $1;
+
+-- name: GetProblemHints :many
+SELECT * FROM problem_hint WHERE problem_id = $1;
+
+-- name: GetProblemByTitle :one
+SELECT * FROM problem WHERE title = $1;
+
 -- name: GetProblemSolutionExpectedOutputHash :one
 SELECT expected_output_hash FROM problem_solution WHERE problem_id = $1;
+
+-- name: CreateProblemSolution :exec
+INSERT INTO problem_solution (problem_id, expected_output, expected_output_hash) VALUES ($1, $2, $3);
--- src/sql/schemas/problems.sql
+++ src/sql/schemas/problems.sql
@@ -1,18 +1,38 @@
+CREATE TYPE problem_language AS ENUM ('cpp', 'go', 'java', 'javascript', 'python', 'typescript');
+
 CREATE TABLE problem (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    hint TEXT[],
+    description TEXT,
+    prompt TEXT NOT NULL,
+    points INT NOT NULL DEFAULT 1,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     tags TEXT[],
-    UNIQUE (id)
+    UNIQUE (id, title)
 );
 
+CREATE TABLE problem_code (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    problem_id UUID REFERENCES problem(id) ON DELETE CASCADE,
+    language problem_language NOT NULL,
+    code BYTEA NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (problem_id, id)
+);
+
+CREATE TABLE problem_hint (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    problem_id UUID REFERENCES problem(id) ON DELETE CASCADE,
+    description BYTEA NOT NULL,
+    answer BYTEA NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (problem_id, id)
+);
 
 CREATE TABLE problem_solution (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    problem_id BIGINT REFERENCES problem(id) ON DELETE CASCADE,
-    expected_output TEXT NOT NULL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    problem_id UUID REFERENCES problem(id) ON DELETE CASCADE,
+    expected_output BYTEA NOT NULL,
     expected_output_hash BYTEA NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (problem_id, id)
