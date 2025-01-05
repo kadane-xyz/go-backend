9ed64f54c6522383abf6ee730b3b7d881737e9db
tonymmm1 <tony@magnatox.com>
2025-01-05 12:03:22 -0600

    change judge0 to handle strings instead of []byte type parameters

---
+++ src/judge0/utils.go
@@ -0,0 +1,45 @@
+package judge0
+
+import "encoding/base64"
+
+func LanguageToLanguageID(language string) int {
+	return languageIDMap[language]
+}
+
+func LanguageIDToLanguage(languageID int) string {
+	for language, id := range languageIDMap {
+		if id == languageID {
+			return language
+		}
+	}
+	return ""
+}
+
+func EncodeBase64(text string) string {
+	return base64.StdEncoding.EncodeToString([]byte(text))
+}
+
+func DecodeBase64(encodedText string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(encodedText)
+	if err != nil {
+		return "", err
+	}
+	return string(decoded), nil
+}
+
+func EncodeSubmissionInputs(submission Submission) Submission {
+	submission.SourceCode = EncodeBase64(submission.SourceCode)
+	submission.Stdin = EncodeBase64(submission.Stdin)
+	submission.ExpectedOutput = EncodeBase64(submission.ExpectedOutput)
+	return submission
+}
+
+func EncodeInputSubmissionsInput(submissions []Submission) []Submission {
+	for i, submission := range submissions {
+		submission.SourceCode = EncodeBase64(submission.SourceCode)
+		submission.Stdin = EncodeBase64(submission.Stdin)
+		submission.ExpectedOutput = EncodeBase64(submission.ExpectedOutput)
+		submissions[i] = submission
+	}
+	return submissions
+}
