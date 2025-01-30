64d8aca83b06aab1594c4333ea267b984dde91a6
tonymmm1 <tony@magnatox.com>
2025-01-30 17:54:04 -0600

    improve docker compose and auto create db tables for docker container

--- src/sql/schemas/room.sql
+++ src/sql/schemas/room.sql
@@ -1,14 +1,20 @@
-CREATE TYPE mode as ENUM ('CLASSIC', 'TEAM');
+CREATE TYPE mode as ENUM ('classic', 'team');
 
 CREATE TABLE room (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    admin uuid NOT NULL REFERENCES account(id),
+    admin text NOT NULL REFERENCES account(id),
     name text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
     max_players int NOT NULL DEFAULT 8,
-    visibility visibility NOT NULL DEFAULT 'PUBLIC',
-    problems uuid[] NOT NULL REFERENCES problem(id),
-    difficulty TEXT NOT NULL DEFAULT 'EASY',
-    mode mode NOT NULL DEFAULT 'CLASSIC',
-    whitelist uuid[] NOT NULL REFERENCES account(id)
+    visibility visibility NOT NULL DEFAULT 'public',
+    difficulty TEXT NOT NULL DEFAULT 'easy',
+    mode mode NOT NULL DEFAULT 'classic',
+    whitelist uuid[] NOT NULL DEFAULT '{}'
+);
+
+CREATE TABLE room_problems (
+    room_id uuid REFERENCES room(id) ON DELETE CASCADE,
+    problem_id int REFERENCES problem(id) ON DELETE CASCADE,
+    problem_order int NOT NULL,
+    PRIMARY KEY (room_id, problem_id)
 );
