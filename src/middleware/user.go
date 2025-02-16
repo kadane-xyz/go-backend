81f7df006072e08f132016e01a370900fb7a614c
tonymmm1 <tony@magnatox.com>
2025-02-15 21:15:07 -0600

    improve UserAuth middleware

--- src/middleware/user.go
+++ src/middleware/user.go
@@ -10,16 +10,22 @@ import (
 func (h *Handler) UserAuth() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			claims := r.Context().Value(ClientTokenKey).(ClientContext)
+			rawClaims := r.Context().Value(ClientTokenKey)
+			claims, ok := rawClaims.(ClientContext)
+			if !ok {
+				apierror.SendError(w, http.StatusUnauthorized, "Unauthorized")
+				return
+			}
 			accountPlan, err := h.PostgresQueries.GetAccountPlan(r.Context(), claims.UserID)
 			if err != nil {
 				apierror.SendError(w, http.StatusForbidden, "Forbidden")
 				return
 			}
 
-			claims.Plan = accountPlan // Set the account type
+			newClaims := claims
+			newClaims.Plan = accountPlan
 
-			ctx := context.WithValue(r.Context(), ClientTokenKey, claims)
+			ctx := context.WithValue(r.Context(), ClientTokenKey, newClaims)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
