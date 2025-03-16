c1e5805e6394d482b6cee17590d388a57e0cbab1
tonymmm1 <tony@magnatox.com>
2025-03-16 18:25:03 -0500

    WIP: Start work on judge0 run testing cases.

---
+++ src/api/run_test.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"net/http"
+	"testing"
+)
+
+// TestCreateRun tests the POST /runs endpoint
+func TestCreateRun(t *testing.T) {
+	testingCases := []TestingCase{
+		{
+			name: "Create run",
+			body: RunRequest{
+				Language:   "go",
+				SourceCode: "func twoSum(nums []int, target int) []int { m := make(map[int]int); for i, num := range nums { m[num] = i }; for i, num := range nums { if j, ok := m[target-num]; ok && j != i { return []int{i, j} } }; return []int{} }",
+				ProblemID:  1,
+				TestCases: []TestCase{
+					{
+						Input: []TestCaseInput{
+							{
+								Name:  "nums",
+								Type:  "int[]",
+								Value: "[2, 7, 11, 15]",
+							},
+							{
+								Name:  "target",
+								Type:  "int",
+								Value: "9",
+							},
+						},
+					},
+					{
+						Input: []TestCaseInput{
+							{
+								Name:  "nums",
+								Type:  "int[]",
+								Value: "[3, 2, 4]",
+							},
+							{
+								Name:  "target",
+								Type:  "int",
+								Value: "6",
+							},
+						},
+					},
+					{
+						Input: []TestCaseInput{
+							{
+								Name:  "nums",
+								Type:  "int[]",
+								Value: "[3, 3]",
+							},
+							{
+								Name:  "target",
+								Type:  "int",
+								Value: "6",
+							},
+						},
+					},
+				},
+			},
+			expectedStatus: http.StatusOK,
+		},
+	}
+
+	for _, tc := range testingCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Build request
+			request := newTestRequestWithBody(t, http.MethodPost, "/runs", tc.body)
+
+			// Execute request
+			executeTestRequest(t, request, tc.expectedStatus, handler.CreateRunRoute)
+		})
+	}
+}
